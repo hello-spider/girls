047a301929f95d874045b69d52d149d5c0c595f6
Meng Xiangzhuo <aumo@foxmail.com>
2021-01-10 04:43:00 +0800

    Add video download in main.py spider (not test).

--- kindgirls_com/kindgirls_com/spiders/main.py
+++ kindgirls_com/kindgirls_com/spiders/main.py
@@ -8,9 +8,7 @@ class MainSpider(Spider):
     allowed_domains = ['kindgirls.com']
 
     def start_requests(self):
-        # yield from self.start_initial_requests()
-        url = 'https://vids.kindgirls.com/dvv9/stella-cardo-2.mp4'
-        yield Request(url, callback=self.save_video)
+        yield from self.start_initial_requests()
 
     def start_initial_requests(self):
         initials = [chr(i) for i in range(ord('a'), ord('z') + 1)]
@@ -27,11 +25,22 @@ class MainSpider(Spider):
         gallery_urls = response.css('.gal_list a::attr(href)').getall()
         gallery_full_urls = [gallery_url.replace('gallery.php', 'gallery-full.php')
                              for gallery_url in gallery_urls]
+        video_page_urls = response.css('.video_list a::attr(href)').getall()
         yield from response.follow_all(gallery_full_urls, callback=self.parse_gallery_full)
+        yield from response.follow_all(video_page_urls, callback=self.parse_video_page)
 
     def parse_gallery_full(self, response):
         img_urls = response.css('.gal_full img::attr(src)').getall()
-        yield from response.follow_all(img_urls, callback=self.save_image)
+        download_urls = []
+
+        for img_url in img_urls:
+            if not os.path.exists(img_url):
+                download_urls.append(img_url)
+        yield from response.follow_all(download_urls, callback=self.save_image)
+
+    def parse_video_page(self, response):
+        video_url = response.css('video source[type=video\/mp4]::attr(src)')
+        yield response.follow(video_url, callback=self.save_video)
 
     def save_file(self, file_path, content):
         try:
@@ -43,16 +52,24 @@ class MainSpider(Spider):
             with open(file_path, 'wb') as f:
                 f.write(content)
 
-    def save_image(self, response):
+    def get_img_path(self, url):
         save_dir = self.settings.get('SAVE_DIR')
-        file_name = response.url.split('/')[-1]
+        file_name = url.split('/')[-1]
         girl_name = file_name.rsplit('_', 2)[0]
         file_path = os.path.join(save_dir, girl_name, file_name)
+        return file_path
+
+    def get_video_path(self, url):
+        save_dir = self.settings.get('SAVE_DIR')
+        video_folder = os.path.join(save_dir, 'videos')
+        file_name = url.split('/')[-1]
+        file_path = os.path.join(video_folder, file_name)
+        return file_path
+
+    def save_image(self, response):
+        file_path = self.get_img_path(response.url)
         self.save_file(file_path, response.body)
 
     def save_video(self, response):
-        save_dir = self.settings.get('SAVE_DIR')
-        video_folder = os.path.join(save_dir, 'videos')
-        file_name = response.url.split('/')[-1]
-        file_path = os.path.join(video_folder, file_name)
+        file_path = self.get_video_path(response.url)
         self.save_file(file_path, response.body)
--- kindgirls_com/kindgirls_com/spiders/videos.py
+++ kindgirls_com/kindgirls_com/spiders/videos.py
@@ -10,10 +10,10 @@ class MainSpider(CrawlSpider):
     start_urls = ['https://www.kindgirls.com/video-archive.php']
     rules = (
         Rule(LinkExtractor(allow=r'https://www\.kindgirls\.com/video-archive\.php')),
-        Rule(LinkExtractor(allow=r'https://www\.kindgirls\.com/video\.php')),
-        Rule(LinkExtractor(allow=r'.*?\.mp4', tags='source', attrs='src', deny_extensions=[]),
-             callback='save_video'),
+        Rule(LinkExtractor(allow=r'https://www\.kindgirls\.com/video\.php'),
+             callback='download_video'),
     )
+    count = 0
 
     def save_file(self, file_path, content):
         try:
@@ -25,9 +25,20 @@ class MainSpider(CrawlSpider):
             with open(file_path, 'wb') as f:
                 f.write(content)
 
-    def save_video(self, response):
+    def get_video_path(self, url):
         save_dir = self.settings.get('SAVE_DIR')
         video_folder = os.path.join(save_dir, 'videos')
-        file_name = response.url.split('/')[-1]
+        file_name = url.split('/')[-1]
         file_path = os.path.join(video_folder, file_name)
+        return file_path
+
+    def download_video(self, response):
+        video_url = response.css('video source[type=video\/mp4]::attr(src)').get()
+        file_path = self.get_video_path(video_url)
+        if os.path.exists(file_path):
+            return
+        yield response.follow(video_url, callback=self.save_video)
+
+    def save_video(self, response):
+        file_path = self.get_video_path(response.url)
         self.save_file(file_path, response.body)
